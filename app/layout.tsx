6c66793abb347f0cd10d66e3683c53963c9fc8bd
Alexandre Chloe <132390197+alexthejumper@users.noreply.github.com>
2025-02-23 17:01:02 +0400

    Added metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,20 @@ import Navbar from "@/app/components/Navbar";
 import Head from "next/head";
 
 export const metadata: Metadata = {
-    title: "Welcome to Our Church",
-    description: "Join us for worship and fellowship every Sunday.",
+    title: "ADD Petite Riviere",
+    description: "Un lieu d’adoration, d’amour et d’unité – venez nous rejoindre ce dimanche !",
+    keywords: [
+        "Église chrétienne", "Assemblée de Dieu", "Petite Rivière",
+        "Culte", "Prière", "Adoration", "Maurice", "Foi", "Bible", "Louange", "Adoration", "Jesus", "Dieu",
+        "Sauveur", "L'Eternel", "Yeshua", "Vie", "Emmanuel", "Amour", "Unité"
+    ],
+
+    robots: "index, follow", // Allows search engines to index and follow links
+
+    icons: {
+        icon: "/icon-add.ico", // Standard favicon
+        shortcut: "/icon-add.ico"
+    },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
