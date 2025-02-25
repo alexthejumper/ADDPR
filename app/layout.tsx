68228ff8c9c643b66858ab6ff2bdc058628c22d4
Alexandre Chloe <132390197+alexthejumper@users.noreply.github.com>
2025-02-25 14:23:28 +0400

    Enforced light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="color-scheme" content="light" />
         </Head>
         <body>
         <Preloader />
