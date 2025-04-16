8081f3c3ba4235bf6d779c969372e4e6cb43fa15
Alexandre Chloe <chloe.alexandre@elca.mu>
2025-04-16 11:09:12 +0400

    Commented the morning prayers weekly events

--- app/weeklyData/default/defaultEvents.ts
+++ app/weeklyData/default/defaultEvents.ts
@@ -1,21 +1,21 @@
 // weeklyEvents.js
 export const weeklyEvents = [
-    {
-        title: "Morning Prayer",
-        description: "A time of prayer to start the day.",
-        dayOfWeek: "Sunday",
-        startTime: "23:00",
-        endTime: "05:00",
-        allDay: false
-    },
-    {
-        title: "Morning Prayer 2",
-        description: "A time of prayer to start the day.",
-        dayOfWeek: "Sunday",
-        startTime: "00:01",
-        endTime: "01:00",
-        allDay: false
-    },
+    // {
+    //     title: "Morning Prayer",
+    //     description: "A time of prayer to start the day.",
+    //     dayOfWeek: "Sunday",
+    //     startTime: "23:00",
+    //     endTime: "05:00",
+    //     allDay: false
+    // },
+    // {
+    //     title: "Morning Prayer 2",
+    //     description: "A time of prayer to start the day.",
+    //     dayOfWeek: "Sunday",
+    //     startTime: "00:01",
+    //     endTime: "01:00",
+    //     allDay: false
+    // },
     {
         title: "Reunion d'Edification",
         description: "Reunion d'Edification.",
